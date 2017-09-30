6032bc30174ae627ddc6708cfd65a2b78264b4f1
Matt Ickstadt <mattico8@gmail.com>
2017-09-30 16:18:10 -0500

    Start implementing TokenInfo

--- src/inverted_index.rs
+++ src/inverted_index.rs
@@ -2,15 +2,49 @@
 use std::collections::HashMap;
 
 // TODO: manually implement serialize so children are inline
-#[derive(Serialize, Debug)]
+#[derive(Serialize, Debug, Clone)]
 pub struct IndexItem {
     docs: HashMap<String, String>,
     df: usize,
     children: HashMap<String, IndexItem>,
 }
 
+pub struct TokenInfo {}
+
+impl IndexItem {
+    pub fn new() -> Self {
+        IndexItem {
+            docs: HashMap::new(),
+            df: 0,
+            children: HashMap::new(),
+        }
+    }
+
+    pub fn add_token(&mut self, token: &str) 
+    {
+        if let Some((idx, char)) = token.char_indices().next() {
+            let item = self.children.entry(char.to_string()).or_insert(IndexItem::new());
+            item.add_token(&token[idx..]);
+        }
+    }
+}
+
 #[derive(Serialize, Debug)]
 pub struct InvertedIndex {
     root: IndexItem,
+}
+
+impl InvertedIndex {
+    pub fn new() -> Self {
+        InvertedIndex {
+            root: IndexItem::new(),
+        }
+    }
+
+    pub fn add_token(&mut self, token: &str, token_info: TokenInfo) 
+    {
+        self.root.add_token(token);
+        // TODO: handle docs, tokeninfo
+    }
 }
 
