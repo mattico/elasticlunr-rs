077d7c4102182f2225bea405afa8e819df76437d
Matt Ickstadt <mattico8@gmail.com>
2017-10-03 22:07:59 -0500

    Add tests for trimmer, stemmer

--- src/lib.rs
+++ src/lib.rs
@@ -42,11 +42,3 @@ impl IndexBuilder {
         serde_json::to_string_pretty(&self.index).unwrap()
     }
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
--- src/pipeline.rs
+++ src/pipeline.rs
@@ -97,4 +97,122 @@ fn stop_word_filter(token: String) -> Option<String> {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
+    macro_rules! pipeline_eq {
+        ($func:expr, $input:expr, $output:expr) => {
+            assert_eq!($func($input.to_string()), Some($output.to_string()));
+        }
+    }
+
+    #[test]
+    fn latin_characters() {
+        pipeline_eq!(trimmer, "hello", "hello");
+    }
+
+    #[test]
+    fn removing_punctuation() {
+        pipeline_eq!(trimmer, "hello.", "hello");
+        pipeline_eq!(trimmer, "it's", "it's");
+        pipeline_eq!(trimmer, "james'", "james");
+        pipeline_eq!(trimmer, "stop!", "stop");
+        pipeline_eq!(trimmer, "first,", "first");
+        pipeline_eq!(trimmer, "", "");
+        pipeline_eq!(trimmer, "[tag]", "tag");
+        pipeline_eq!(trimmer, "[[[tag]]]", "tag");
+        pipeline_eq!(trimmer, "[[!@#@!hello]]]}}}", "hello");
+        pipeline_eq!(trimmer, "~!@@@hello***()()()]]", "hello");
+    }
+
+    #[test]
+    fn test_stemmer() {
+        let cases = [
+            ("consign", "consign"),
+            ("consigned", "consign"),
+            ("consigning", "consign"),
+            ("consignment", "consign"),
+            ("consist", "consist"),
+            ("consisted", "consist"),
+            ("consistency", "consist"),
+            ("consistent", "consist"),
+            ("consistently", "consist"),
+            ("consisting", "consist"),
+            ("consists", "consist"),
+            ("consolation", "consol"),
+            ("consolations", "consol"),
+            ("consolatory", "consolatori"),
+            ("console", "consol"),
+            ("consoled", "consol"),
+            ("consoles", "consol"),
+            ("consolidate", "consolid"),
+            ("consolidated", "consolid"),
+            ("consolidating", "consolid"),
+            ("consoling", "consol"),
+            ("consols", "consol"),
+            ("consonant", "conson"),
+            ("consort", "consort"),
+            ("consorted", "consort"),
+            ("consorting", "consort"),
+            ("conspicuous", "conspicu"),
+            ("conspicuously", "conspicu"),
+            ("conspiracy", "conspiraci"),
+            ("conspirator", "conspir"),
+            ("conspirators", "conspir"),
+            ("conspire", "conspir"),
+            ("conspired", "conspir"),
+            ("conspiring", "conspir"),
+            ("constable", "constabl"),
+            ("constables", "constabl"),
+            ("constance", "constanc"),
+            ("constancy", "constanc"),
+            ("constant", "constant"),
+            ("knack", "knack"),
+            ("knackeries", "knackeri"),
+            ("knacks", "knack"),
+            ("knag", "knag"),
+            ("knave", "knave"),
+            ("knaves", "knave"),
+            ("knavish", "knavish"),
+            ("kneaded", "knead"),
+            ("kneading", "knead"),
+            ("knee", "knee"),
+            ("kneel", "kneel"),
+            ("kneeled", "kneel"),
+            ("kneeling", "kneel"),
+            ("kneels", "kneel"),
+            ("knees", "knee"),
+            ("knell", "knell"),
+            ("knelt", "knelt"),
+            ("knew", "knew"),
+            ("knick", "knick"),
+            ("knif", "knif"),
+            ("knife", "knife"),
+            ("knight", "knight"),
+            ("knights", "knight"),
+            ("knit", "knit"),
+            ("knits", "knit"),
+            ("knitted", "knit"),
+            ("knitting", "knit"),
+            ("knives", "knive"),
+            ("knob", "knob"),
+            ("knobs", "knob"),
+            ("knock", "knock"),
+            ("knocked", "knock"),
+            ("knocker", "knocker"),
+            ("knockers", "knocker"),
+            ("knocking", "knock"),
+            ("knocks", "knock"),
+            ("knopp", "knopp"),
+            ("knot", "knot"),
+            ("knots", "knot"),
+            ("lay", "lay"),
+            ("try", "tri"),
+        ];
+
+        for &(input, output) in cases.iter() {
+            pipeline_eq!(stemmer, input, output);
+        }
+    }
+}
