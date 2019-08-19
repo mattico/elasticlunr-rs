ca26173f37cf5eb1153078105f26a71ea3b4f421
liukz <liukz@moresec.cn>
2019-08-19 20:36:33 +0800

    feat: add Chinese language
    add Chinese trimmer

--- src/lang/zh.rs
+++ src/lang/zh.rs
@@ -11,14 +11,48 @@ pub fn make_pipeline() -> Pipeline {
     }
 }
 
+
 pub fn trimmer(token: String) -> Option<String> {
-    Some(token)
+    println!("trim {}", token);
+
+    for c in token.chars() {
+        println!("{}, {}", c, c as u32);
+    }
+
+    let ret: String = token.
+        trim_matches(|c: char| !is_valid_char(c)  )
+        .into();
+
+    println!("end trim{}", ret);
+
+    if ret.eq("") {
+        return None;
+    }
+
+    Some(ret)
 }
 
 make_stop_word_filter!([
-    ""
+    "的", "了"
 ]);
 
 fn stemmer(token: String) -> Option<String> {
     Some(token)
+}
+
+fn is_valid_char(c: char) -> bool {
+    let min_max_list = [
+        [19668, 40869], // min and max Chinese char
+        ['a' as u32, 'z' as u32],
+        ['A' as u32, 'Z' as u32]
+    ];
+
+    let c = c as u32;
+    for min_max in min_max_list.iter() {
+        if c >= min_max[0] && c <= min_max[1] {
+            return true;
+        }
+    }
+
+    false
 }
