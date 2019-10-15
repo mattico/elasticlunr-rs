379309466c1a19973e73fcbfab5949b540e723fa
liukz <liukz@moresec.cn>
2019-10-15 14:26:34 +0800

    refact: rm unused debug code

--- tests/test-compare.rs
+++ tests/test-compare.rs
@@ -65,10 +65,7 @@ fn get_fixture() -> serde_json::Value {
 fn search_index_hasnt_changed_accidentally() {
     let new_index = create_index();
     let fixture_index = get_fixture();
-
-    println!("{}", &new_index);
-    println!("{}", &fixture_index);
-
+ 
     if new_index != fixture_index {
         panic!("The search index has changed from the fixture");
     }
