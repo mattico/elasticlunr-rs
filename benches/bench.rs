4fe78e15fb19b992b2e99f856506c0404d6a3028
Matt Ickstadt <mattico8@gmail.com>
2022-05-21 23:31:25 -0500

    Remove old benches

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,5 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use elasticlunr::{lang, pipeline::tokenize, Index};
+use elasticlunr::Index;
 
 fn bench_main(c: &mut Criterion) {
     // BTreeMap<String, IndexItem>: 3,165,389 ns/iter (+/- 420,869)
@@ -14,39 +14,6 @@ fn bench_main(c: &mut Criterion) {
             }
             black_box(index.to_json());
         })
-    });
-
-    // PHF:           2,507,473 ns/iter (+/- 197,005)
-    // Linear search: 2,481,133 ns/iter (+/- 216,998)
-    c.bench_function("stemmer", |b| {
-        let text = include_str!("../tests/data/en.in.txt");
-        let tokens = tokenize(text);
-        b.iter(|| {
-            let tokens = tokens.clone();
-            for token in tokens {
-                black_box(lang::en::stemmer(token));
-            }
-        });
-    });
-
-    // HashSet:  175,669 ns/iter (+/- 15,652)
-    // BTreeSet: 210,169 ns/iter (+/- 29,430)
-    // PHF:      159,961 ns/iter (+/- 16,492)
-    #[cfg(feature = "fr")]
-    c.bench_function("stop_word_fr", |b| {
-        let text = include_str!("../tests/data/fr.in.txt");
-        let tokens = tokenize(text);
-        let tokens: Vec<_> = tokens
-            .into_iter()
-            .filter_map(|t| lang::fr::trimmer(t))
-            .collect();
-
-        b.iter(|| {
-            let tokens = tokens.clone();
-            for token in tokens {
-                black_box(lang::fr::stop_word_filter(token));
-            }
-        });
     });
 }
 
