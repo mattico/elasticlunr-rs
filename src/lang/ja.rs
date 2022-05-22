4c8d4450cc794cedb550c0fdb9cdc33e88e3dc6c
Matt Ickstadt <mattico8@gmail.com>
2022-05-22 12:50:39 -0500

    ja: use Unicode character classes
    trimming the length marks seems like an improvement

--- src/lang/ja.rs
+++ src/lang/ja.rs
@@ -1,4 +1,4 @@
-use super::Language;
+use super::{common::RegexTrimmer, Language};
 use crate::pipeline::{FnWrapper, Pipeline};
 use lindera::tokenizer::{Tokenizer, TokenizerConfig};
 use lindera_core::viterbi::Mode;
@@ -46,72 +46,29 @@ impl Language for Japanese {
     fn make_pipeline(&self) -> Pipeline {
         Pipeline {
             queue: vec![
-                Box::new(FnWrapper("trimmer-ja".into(), trimmer)),
+                Box::new(RegexTrimmer::new("trimmer-ja".into(), WORD_CHARS)),
                 Box::new(FnWrapper("stemmer-ja".into(), stemmer)),
             ],
         }
     }
 }
 
-pub fn trimmer(token: String) -> Option<String> {
-    match token.trim_matches(|c| !is_valid_char(c)) {
-        x if x.is_empty() => None,
-        x => Some(x.to_string()),
-    }
-}
+const WORD_CHARS: &'static str = r"0-9A-Za-z\p{Hiragana}\p{Katakana}\p{Unified_Ideograph}";
 
 fn stemmer(token: String) -> Option<String> {
     Some(token)
 }
 
-fn is_valid_char(c: char) -> bool {
-    let min_max_list = [
-        ('0' as u32, '9' as u32),
-        ('a' as u32, 'z' as u32),
-        ('A' as u32, 'Z' as u32),
-        // the following ranges are where Japanese characters are placed
-        ('\u{3041}' as u32, '\u{30A0}' as u32),
-        ('\u{30A0}' as u32, '\u{30FF}' as u32),
-        ('\u{31F0}' as u32, '\u{31FF}' as u32),
-        ('\u{3099}' as u32, '\u{309C}' as u32),
-        ('\u{4E00}' as u32, '\u{9FCf}' as u32),
-        ('\u{F900}' as u32, '\u{FAFF}' as u32),
-        ('\u{3400}' as u32, '\u{4DBF}' as u32),
-    ];
-    let c = c as u32;
-    min_max_list.iter().any(|&(min, max)| min <= c && c <= max)
-}
-
 #[cfg(test)]
 mod tests {
+    use crate::pipeline::PipelineFn;
     use super::*;
 
     #[test]
-    fn test_is_valid_char() {
-        assert!(is_valid_char('0'));
-        assert!(is_valid_char('9'));
-        assert!(is_valid_char('a'));
-        assert!(is_valid_char('z'));
-        assert!(is_valid_char('A'));
-        assert!(is_valid_char('Z'));
-        assert!(is_valid_char('あ'));
-        assert!(is_valid_char('ん'));
-        assert!(is_valid_char('ア'));
-        assert!(is_valid_char('ン'));
-        assert!(is_valid_char('亜'));
-        assert!(is_valid_char('日'));
-        assert!(is_valid_char('本'));
-        assert!(is_valid_char('語'));
-        assert!(is_valid_char('ー'));
-        assert!(!is_valid_char('。'));
-        assert!(!is_valid_char('！'));
-        assert!(!is_valid_char('〜'));
-    }
-
-    #[test]
     fn test_trimmer() {
+        let trimmer = RegexTrimmer::new("trimmer-ja".into(), WORD_CHARS);
         assert_eq!(
-            trimmer("  こんにちは、世界！".to_string()),
+            trimmer.filter("  こんにちは、世界！".to_string()),
             Some("こんにちは、世界".to_string())
         );
     }
